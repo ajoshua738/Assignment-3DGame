c18a00626a08ff58aa169b073f9645c5acc34e55
Adrian Joshua <adrianjoshua738@gmail.com>
2022-12-02 17:58:55 +0800

    Basic env and player
    Created new scene to work on
    Added FPS controller
    Created very basic indoor env

---
+++ 3DGameAssignment/Assets/Joshua_Assets/Flashlight/Flashlight.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Flashlight : MonoBehaviour
+{
+    public GameObject ON;
+    public GameObject OFF;
+    private bool isOn;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ON.SetActive(false);
+        OFF.SetActive(true);
+        isOn = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            if (isOn)
+            {
+                ON.SetActive(false);
+                OFF.SetActive(true);
+            }
+
+            if (!isOn)
+            {
+                ON.SetActive(true);
+                OFF.SetActive(false);
+            }
+            isOn = !isOn;
+        }
+    }
+}
