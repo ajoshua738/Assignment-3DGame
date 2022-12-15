75dd8c9e7b0d4d1d71fa1bbc08cdba6d9d6a24c4
SHMAN123456 <shong-wm20@student.tarc.edu.my>
2022-12-15 12:47:17 +0800

    Maybe
    Complete

---
+++ 3DGameAssignment/Assets/TexturePuzzle/OpenSceneDoors.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class OpenSceneDoors : MonoBehaviour
+{
+    public Animator door;
+    public GameObject openText;
+
+    public AudioSource doorSound;
+
+
+    public bool inReach;
+
+
+
+
+    void Start()
+    {
+        inReach = false;
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Reach")
+        {
+            inReach = true;
+            openText.SetActive(true);
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "Reach")
+        {
+            inReach = false;
+            openText.SetActive(false);
+        }
+    }
+
+
+
+
+
+    void Update()
+    {
+
+        if (inReach && Input.GetButtonDown("Interact"))
+        {
+            DoorOpens();
+        }
+
+        else
+        {
+            DoorCloses();
+        }
+
+
+
+
+    }
+    void DoorOpens ()
+    {
+        Debug.Log("It Opens");
+        door.SetBool("Open", true);
+        door.SetBool("Closed", false);
+        doorSound.Play();
+        SceneManager.LoadScene("OpenJumpScare", LoadSceneMode.Single);
+
+    }
+
+    void DoorCloses()
+    {
+        Debug.Log("It Closes");
+        door.SetBool("Open", false);
+        door.SetBool("Closed", true);
+    }
+
+
+}
